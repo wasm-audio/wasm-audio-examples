ff6dfb1b307689a084093ae2bce013f82670e669
Qichao Lan <chaosprint@gmail.com>
2024-06-06 23:21:31 +0200

    add some macros

--- guest-sin/src/lib.rs
+++ guest-sin/src/lib.rs
@@ -1,52 +1,44 @@
+mod prelude;
+use prelude::*;
+
 #[allow(warnings)]
 mod bindings;
 
 use bindings::Guest;
 
-use hashbrown::HashMap;
-use lazy_static::lazy_static;
-use parking_lot::Mutex;
+def_param!(PHASE, 0.0);
+def_param!(FREQUENCY, 440.0);
+def_param!(SAMPLE_RATE, 48000.0);
 
-pub type Parameters = HashMap<String, f32>;
-
-lazy_static! {
-    // static ref PHASE: Mutex<f32> = Mutex::new(0.0);
-    // static ref FREQUENCY: Mutex<f32> = Mutex::new(440.0);
-    // static ref SAMPLE_RATE: Mutex<f32> = Mutex::new(48000.0);
-    static ref PARAMETES: Mutex<Parameters> = Mutex::new(Parameters::from_iter(
-        [
-            ("phase".to_string(), 0.0),
-            ("frequency".to_string(), 440.0),
-            ("sample_rate".to_string(), 48000.0),
-        ]
-    ));
-}
-
-struct Component;
+pub struct Component;
 
 impl Guest for Component {
     fn set(key: String, value: f32) {
-        let mut parameters = PARAMETES.lock();
-        parameters.insert(key, value);
+        match key.as_str() {
+            "phase" => set_param!(PHASE, value),
+            "frequency" => set_param!(FREQUENCY, value),
+            "sample_rate" => set_param!(SAMPLE_RATE, value),
+            _ => (),
+        }
     }
-    fn process(input: Vec<f32>) -> Vec<f32> {
-        let mut parameters = PARAMETES.lock();
 
-        let frequency = *parameters.get_mut("frequency").unwrap();
-        let sample_rate = *parameters.get_mut("sample_rate").unwrap();
-        let mut phase = *parameters.get_mut("phase").unwrap();
+    fn process(input: Vec<f32>) -> Vec<f32> {
+        let frequency = get_param!(FREQUENCY);
+        let sample_rate = get_param!(SAMPLE_RATE);
+        let mut phase = get_param!(PHASE);
         let delta_phase = frequency / sample_rate;
 
         let mut output = Vec::with_capacity(input.len());
-        // println!("input: {:?}", input);
         for _ in 0..input.len() {
             phase += delta_phase;
             if phase > 1.0 {
                 phase -= 1.0;
             }
-            parameters.insert("phase".to_string(), phase);
             output.push((phase * 2.0 * std::f32::consts::PI).sin());
         }
+
+        set_param!(PHASE, phase);
+
         output
     }
 }
---
+++ guest-sin/src/prelude.rs
@@ -0,0 +1,26 @@
+pub use atomic_float::AtomicF32;
+pub use lazy_static::lazy_static;
+pub use std::sync::atomic::Ordering;
+
+#[macro_export]
+macro_rules! def_param {
+    ($name:ident, $value:expr) => {
+        lazy_static! {
+            static ref $name: AtomicF32 = AtomicF32::new($value);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! set_param {
+    ($name:ident, $value:expr) => {
+        $name.store($value, Ordering::SeqCst)
+    };
+}
+
+#[macro_export]
+macro_rules! get_param {
+    ($name:ident) => {
+        $name.load(Ordering::SeqCst)
+    };
+}
